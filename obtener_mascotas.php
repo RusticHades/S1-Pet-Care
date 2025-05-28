da38b981c7c3c90a70e81460687bb3b46af1e393
RusticHades1578 <christiangarduno292@gmail.com>
2025-05-28 15:42:02 -0600

    Se agrega obtener mascotas

---
+++ obtener_mascotas.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Configuración de la BD
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "miapp_db";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$id_usuario = isset($_GET['id_usuario']) ? intval($_GET['id_usuario']) : 2;
+
+$sql = "SELECT id_mascota, nombre, especie, raza,  edad, sexo  FROM MASCOTAS WHERE id_usuario = $id_usuario";
+$result = $conn->query($sql);
+
+$mascotas = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $mascotas[] = $row;
+    }
+}
+
+$conn->close();
+
+echo json_encode($mascotas);
+?>
